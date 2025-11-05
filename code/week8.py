41b24d9119852297738e3b8ffeaa2f731f5e123d
Tyler <tylerbrady2005@gmail.com>
2025-11-05 14:42:50 +0000

    yur

--- code/week8.py
+++ code/week8.py
@@ -14,24 +14,28 @@ def data(word):
    
 def cleaned_words():
     
-    text = data('../data/aceventura.txt')
+    text = data('data/aceventura.txt')
     for word in text.split():
         cleaned_word = ''.join(char for char in word if char.isalnum())
         print(cleaned_word.lower()) 
 
 def top3wordcount():
-    text = data('../data/aceventura.txt')
+    text = data('data/aceventura.txt')
+
+
     word_count = {}
+
     for lines in text:
         words = lines.split()
 
         for word in words:
             word = word.lower().strip('.,!?;"()[]')
+
             word_count[word] = word_count.get(word, 0) + 1
 
 
     top3 = sorted(word_count.items(), key=lambda x: x[1], reverse=True)[:3]
-    
+
     print("Top 3 most frequent words:")
     for word, count in top3:
         print(f"{word}: {count} times") 
